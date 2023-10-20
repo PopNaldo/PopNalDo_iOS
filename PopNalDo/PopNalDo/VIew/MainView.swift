dae2a35c5bdaeb58785aa6c7a516db2c55ce508c
Vector_scope <gsball399@gmail.com>
2023-10-20 14:43:15 +0900

    폴더 추가

---
+++ PopNalDo/PopNalDo/VIew/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  PopNalDo
+//
+//  Created by dgsw8th16 on 10/20/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, BongSeong!")
+    }
+}
+
+#Preview {
+    MainView()
+}
