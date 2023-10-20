5370dde1a5caa950e147ac2374bc0a458c7164c8
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-10-20 14:17:31 +0900

    🎉 :: firstCommit

---
+++ PopNalDo/PopNalDo/PopNalDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopNalDoApp.swift
+//  PopNalDo
+//
+//  Created by 최시훈 on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PopNalDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
